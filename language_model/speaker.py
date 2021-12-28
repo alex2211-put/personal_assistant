02fe564a73d626b4244a1eea9f2f4fc88d46a4db
alik_put <56935749+alex2211-put@users.noreply.github.com>
2021-12-28 22:52:24 +0300

    add assistant

---
+++ language_model/speaker.py
@@ -0,0 +1,28 @@
+from . import language_consts
+import pyttsx3
+
+
+class Speaker:
+
+    def __init__(self, language='ru', gender='w'):
+        self.language = language
+        self.engine = pyttsx3.init()
+        self.gender = gender
+
+        self.change_language(language)
+
+    def change_language(self, language):
+        self.language = language
+        if self.gender == 'w':
+            self.engine.setProperty('voice', language_consts.VOICES[language][0])
+        else:
+            self.engine.setProperty('voice', language_consts.VOICES[language][-1])
+
+    def change_gender(self, gender):
+        self.gender = gender
+
+        self.change_language(self.language)
+
+    def speak(self, text):
+        self.engine.say(text)
+        self.engine.runAndWait()
